7fa6d71218d39fb1f3d355680d169a477b4c44ab
Kamil <kamil.dzietczyk1994@gmail.com>
2023-06-29 17:40:10 +0200

    Rebasing

--- app/src/main/java/pl/futurecollars/invoicing/controller/InvoiceController.java
+++ app/src/main/java/pl/futurecollars/invoicing/controller/InvoiceController.java
@@ -3,7 +3,6 @@ package pl.futurecollars.invoicing.controller;
 import java.io.IOException;
 import java.util.List;
 import java.util.Optional;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -14,49 +13,80 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import pl.futurecollars.invoicing.db.memory.InMemoryDatabase;
+import pl.futurecollars.invoicing.jsondatabase.JsonDatabase;
 import pl.futurecollars.invoicing.model.Invoice;
-import pl.futurecollars.invoicing.service.InvoiceService;
 
 @RestController
 @RequestMapping("invoice")
 public class InvoiceController {
+  private final InMemoryDatabase inMemoryDatabase;
+  private final JsonDatabase jsonDatabase;
 
-  private final InvoiceService invoiceService;
-
-  @Autowired
-  public InvoiceController(InvoiceService invoiceService) {
-    this.invoiceService = invoiceService;
+  public InvoiceController() {
+    this.inMemoryDatabase = new InMemoryDatabase();
+    this.jsonDatabase = new JsonDatabase();
   }
 
-  @GetMapping()
+  @GetMapping("/Memory")
   public ResponseEntity<List<Invoice>> getAllInvoicesMemory() throws IOException {
-    List<Invoice> invoices = invoiceService.getAll();
+    List<Invoice> invoices = inMemoryDatabase.getAll();
     return ResponseEntity.ok(invoices);
   }
 
-  @GetMapping("/{id}")
-  public ResponseEntity<Invoice> getInvoiceByIdMemory(@PathVariable("id") int id) throws IOException {
-    Optional<Invoice> invoice = invoiceService.getById(id);
+  @GetMapping("/File")
+  public ResponseEntity<List<Invoice>> getAllInvoicesFile() throws IOException {
+    List<Invoice> invoices = jsonDatabase.getAll();
+    return ResponseEntity.ok(invoices);
+  }
+
+  @GetMapping("/Memory/{id}")
+  public ResponseEntity<Invoice> getInvoiceByIdMemory(@PathVariable("id") int id) {
+    Optional<Invoice> invoice = inMemoryDatabase.getById(id);
     return invoice.map(ResponseEntity::ok)
         .orElse(ResponseEntity.notFound().build());
   }
 
-  @PostMapping("/add")
-  public ResponseEntity<Integer> addInvoiceMemory(@RequestBody Invoice invoice) throws IOException {
-    int id = invoiceService.save(invoice);
+  @GetMapping("/File/{id}")
+  public ResponseEntity<Invoice> getInvoiceByIdFile(@PathVariable("id") int id) throws IOException {
+    Optional<Invoice> invoice = jsonDatabase.getById(id);
+    return invoice.map(ResponseEntity::ok)
+        .orElse(ResponseEntity.notFound().build());
+  }
+
+  @PostMapping("/Memory/add")
+  public ResponseEntity<Integer> addInvoiceMemory(@RequestBody Invoice invoice) {
+    int id = inMemoryDatabase.save(invoice);
     return ResponseEntity.status(HttpStatus.CREATED).body(id);
   }
 
-  @PutMapping("/put/{id}")
+  @PostMapping("/File/add")
+  public ResponseEntity<Integer> addInvoiceFile(@RequestBody Invoice invoice) throws IOException {
+    int id = jsonDatabase.save(invoice);
+    return ResponseEntity.status(HttpStatus.CREATED).body(id);
+  }
+
+  @PutMapping("/Memory/put/{id}")
   public ResponseEntity<Void> updateInvoiceMemory(@PathVariable("id") int id, @RequestBody Invoice updatedInvoice) throws IOException {
-    invoiceService.update(id, updatedInvoice);
+    inMemoryDatabase.update(id, updatedInvoice);
     return ResponseEntity.noContent().build();
   }
 
-  @DeleteMapping("/delete/{id}")
+  @PutMapping("/File/put/{id}")
+  public ResponseEntity<Void> updateInvoiceFile(@PathVariable("id") int id, @RequestBody Invoice updatedInvoice) throws IOException {
+    jsonDatabase.update(id, updatedInvoice);
+    return ResponseEntity.noContent().build();
+  }
+
+  @DeleteMapping("/Memory/delete/{id}")
   public ResponseEntity<Void> deleteInvoiceMemory(@PathVariable("id") int id) throws IOException {
-    invoiceService.delete(id);
+    inMemoryDatabase.delete(id);
     return ResponseEntity.noContent().build();
   }
 
+  @DeleteMapping("/File/delete/{id}")
+  public ResponseEntity<Void> deleteInvoiceFile(@PathVariable("id") int id) throws IOException {
+    jsonDatabase.delete(id);
+    return ResponseEntity.noContent().build();
+  }
 }
