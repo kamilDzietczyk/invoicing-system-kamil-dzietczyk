50f270178272f4f197cd1c4bb18bb307de45f540
Kamil <kamil.dzietczyk1994@gmail.com>
2023-07-07 20:08:04 +0200

    Checkstyle

--- app/src/main/java/pl/futurecollars/invoicing/service/InvoiceService.java
+++ app/src/main/java/pl/futurecollars/invoicing/service/InvoiceService.java
@@ -1,5 +1,6 @@
 package pl.futurecollars.invoicing.service;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.Optional;
 import pl.futurecollars.invoicing.db.Database;
@@ -12,23 +13,23 @@ public class InvoiceService {
     this.database = database;
   }
 
-  public Optional<Invoice> getById(int id) {
+  public Optional<Invoice> getById(int id) throws IOException {
     return database.getById(id);
   }
 
-  public List<Invoice> getAll() {
+  public List<Invoice> getAll() throws IOException {
     return database.getAll();
   }
 
-  public int save(Invoice invoice) {
+  public int save(Invoice invoice) throws IOException {
     return database.save(invoice);
   }
 
-  public void update(int id, Invoice updatedInvoice) {
+  public void update(int id, Invoice updatedInvoice) throws IOException {
     database.update(id, updatedInvoice);
   }
 
-  public void delete(int id) {
+  public void delete(int id) throws IOException {
     database.delete(id);
   }
 }
