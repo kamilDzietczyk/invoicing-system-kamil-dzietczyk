231d1f4c8f2319cf9b744ff5a9144892cc7d8099
Kamil <kamil.dzietczyk1994@gmail.com>
2023-07-07 19:39:49 +0200

    Correct Rebasing

--- app/src/main/java/pl/futurecollars/invoicing/App.java
+++ app/src/main/java/pl/futurecollars/invoicing/App.java
@@ -4,13 +4,12 @@
 
 package pl.futurecollars.invoicing;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
 public class App {
+  public String getGreeting() {
+    return "Hello World!";
+  }
 
   public static void main(String[] args) {
-    SpringApplication.run(App.class, args);
+    System.out.println(new App().getGreeting());
   }
 }
