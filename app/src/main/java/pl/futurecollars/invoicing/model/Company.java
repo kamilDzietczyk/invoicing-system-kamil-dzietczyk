b0219be3be7344b2a3b9f57f5cd86ea65e836b98
kamilDzietczyk <43826839+kamilDzietczyk@users.noreply.github.com>
2023-07-07 18:20:44 +0200

    Task 2 (#2)
    * First Commit
    
    * First Commit
    
    * First Commit
    
    * First Commit

--- app/src/main/java/pl/futurecollars/invoicing/model/Company.java
+++ app/src/main/java/pl/futurecollars/invoicing/model/Company.java
@@ -1,16 +1,11 @@
 package pl.futurecollars.invoicing.model;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Data
-@NoArgsConstructor
-@AllArgsConstructor
 public class Company {
 
   private String taskIdentificationNumber;
   private String address;
   private String name;
-
 }
